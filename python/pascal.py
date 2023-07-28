e7b4ff989a13c6d113a9dfdd78f3b257b079a98a
Federico Frontini <federico.frontini@lantechlongwave.it>
2023-07-28 14:38:58 +0200

    feat: added a better version of pascal.py

--- python/pascal.py
+++ python/pascal.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
+
         #the idea is to use an output 2d array
         output = []
+
         for i in range(numRows):
           #we manually populate the first two rows
           if i == 0:
@@ -18,4 +16,17 @@ class Solution(object):
             for indice in range(len(prev)-1): #we don't want to go out of bounds
                 curr.insert(-1, prev[indice] + prev[indice+1])
             output.append(curr) 
+
         return output
+    
+    #this one has a much better runtime but still uses a lot of memory...
+    def betterGenerate(self, numRows):
+        output = [[1]]
+        for i in range(2, numRows+1):
+          curr = [1]*i #I already know how many elements this row is gonna get
+          for j in range(1, i-1):
+            curr[j] = output[i-2][j]+output[i-2][j-1]
+        output.append(curr)
+        return output
+
+
